3ffd8086fc834efba543cbbd4b02338bf91b6582
kimdk121 <96677617+kimdk121@users.noreply.github.com>
2025-03-18 17:10:26 +0900

    프로젝트 첫 등록

---
+++ src/main/java/com/mangopuree/MangopureeApplication.java
@@ -0,0 +1,13 @@
+package com.mangopuree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangopureeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangopureeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mangopuree/MangopureeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangopuree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangopureeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
