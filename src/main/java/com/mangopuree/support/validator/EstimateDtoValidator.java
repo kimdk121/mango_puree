4aa7142ab0beee066512ba0a7b7802d072144c31
kimdk121 <kimdk121@naver.com>
2025-04-09 23:08:36 +0900

    - 견적서 수정 페이지 작업완료

---
+++ src/main/java/com/mangopuree/support/validator/EstimateDtoValidator.java
@@ -0,0 +1,40 @@
+package com.mangopuree.support.validator;
+
+import com.mangopuree.estimate.dto.EstimateInsertDto;
+import com.mangopuree.estimateitem.dto.EstimateItemInsertDto;
+import jakarta.validation.ConstraintViolation;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.util.Set;
+
+@Component
+public class EstimateDtoValidator implements Validator {
+
+    private final jakarta.validation.Validator validator;
+
+    public EstimateDtoValidator(jakarta.validation.Validator validator) {
+        this.validator = validator;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return EstimateInsertDto.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+        EstimateInsertDto estimateInsertDto = (EstimateInsertDto) target;
+
+        Set<ConstraintViolation<Object>> violations = validator.validate(target);
+
+        for (ConstraintViolation<Object> violation : violations) {
+            String fieldName = violation.getPropertyPath().toString();
+            String messageCode = violation.getMessageTemplate().replaceAll("[{}]", "").split("\\.")[3];
+            errors.rejectValue(fieldName, messageCode);
+        }
+
+    }
+}
